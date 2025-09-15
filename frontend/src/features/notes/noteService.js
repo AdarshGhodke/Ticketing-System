6ac3e3962b7d27030afc6d0b9316bcbda9484182
Adarsh Ghodke <127655943+AdarshGhodke@users.noreply.github.com>
2025-09-15 11:06:36 +0530

    Update noteService.js

--- frontend/src/features/notes/noteService.js
+++ frontend/src/features/notes/noteService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "/api/tickets/";
+// const API_URL = "/api/tickets/";
+const API_URL = process.env.REACT_APP_API_URL + '/api/users/'
 
 // Get ticket notes
 const getNotes = async (ticketId, token) => {
